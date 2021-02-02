bfd39651eceffa0d0768c0ba694d4255e1a7a4fd
Karla Jara <jara.karla.43@gmail.com>
2021-02-02 11:24:58 -0700

    feat: completed exercise question in part 1

--- js/external.js
+++ js/external.js
@@ -1,1 +1,5 @@
 console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+var userInput = prompt('What is your favorite color?');
+alert('Great, ' + userInput + ' is my favorite color too!');
+
