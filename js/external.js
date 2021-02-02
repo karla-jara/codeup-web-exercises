dcd1bee8896805df45d1671791deda43dc9fda70
Karla Jara <jara.karla.43@gmail.com>
2021-02-02 15:06:10 -0700

    feat: finished most of exercise 3

--- js/external.js
+++ js/external.js
@@ -3,3 +3,36 @@ alert("Welcome to my Website!");
 var userInput = prompt('What is your favorite color?');
 alert('Great, ' + userInput + ' is my favorite color too!');
 
+var lm = parseInt(prompt("How many days would you like to rent the Little Mermaid?"));
+var bb = parseInt(prompt("How many days would you like to rent Brother Bear?"));
+var h = parseInt(prompt("How many days would you like to rent Hercules?"));
+
+var price = 3;
+
+lm *= price;
+bb *= price;
+h *= price;
+
+var total = alert("Your total rental price is " + (lm + bb + h));
+
+var google = parseInt(prompt("How many hours did you work for Google?"));
+var amazon = parseInt(prompt("How many hours did you work for Amazon?"));
+var facebook = parseInt(prompt("How many hours did you work for Facebook?"));
+
+google *= 400;
+amazon *= 380;
+facebook *= 350;
+
+var totalPay = alert("Your total pay is " + (google + amazon + facebook));
+
+var classFull = confirm("Is this class full?");
+var scheduleConflict = confirm("Is there a conflict with your schedule?");
+if ((classFull === false) && (scheduleConflict === false)) {
+    alert("Great news, you can enroll!");
+}
+else {
+    alert("Sorry you may not enroll at this time :(");
+}
+
+var purchasedItems = parseInt(confirm("How many items will you purchase today?"));
+var offerExpired = confirm("Has offer expired?")
