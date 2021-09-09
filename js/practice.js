93ab8f52c4aa4e6073f793a50683f4c594570893
Karla Jara <jara.karla.43@gmail.com>
2021-09-08 23:57:59 -0500

    added practice js problem

--- js/practice.js
+++ js/practice.js
@@ -123,3 +123,14 @@ function toArray(obj) {
     }
     return result;
 }
+
+
+// array manipulation
+function arrayOfMultiples (num, length) {
+    var result = []
+    for (let i = 1; i <= length; i++) {
+        result.push(num*i)
+
+    }
+    return result
+}
